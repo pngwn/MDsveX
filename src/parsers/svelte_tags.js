fd969dcfb917213c988d0cac0f5a0e2114997788
pngwn <hello@pngwn.io>
2019-10-04 16:05:35 +0100

    More tests. In a helpful turn of events Jest, has decided to stop working.

--- src/parsers/svelte_tags.js
+++ src/parsers/svelte_tags.js
@@ -1,23 +1,23 @@
 const RE_SVELTE_SELFCLOSING =
-  '\\s*<svelte:([a-z]+) (?:(?:[a-z:]+(?:="*[a-z]+"*)*)\\s*)*\\/>';
+	'\\s*<svelte:([a-z]+) (?:(?:[a-z:]+(?:="*[a-z]+"*)*)\\s*)*\\/>';
 const RE_SVELTE_START =
-  '\\s*<svelte:([a-z]+)\\s*(?:(?:[a-z:]+(?:="*[a-z]+"*)*)\\s*)*>';
+	'\\s*<svelte:([a-z]+)\\s*(?:(?:[a-z:]+(?:="*[a-z]+"*)*)\\s*)*>';
 const RE_SVELTE_END = '\\s*<\\/\\s*svelte:([a-z]+)\\s*>';
 
 const RE_SVELTE_TAG = new RegExp(
-  RE_SVELTE_SELFCLOSING + '|' + RE_SVELTE_START + '|' + RE_SVELTE_END
+	RE_SVELTE_SELFCLOSING + '|' + RE_SVELTE_START + '|' + RE_SVELTE_END
 );
 
 export function parse_svelte_tag(eat, value, silent) {
-  const match = RE_SVELTE_TAG.exec(value);
+	const match = RE_SVELTE_TAG.exec(value);
 
-  if (match) {
-    if (silent) return true;
+	if (match) {
+		if (silent) return true;
 
-    return eat(match[0])({
-      type: 'svelteTag',
-      value: match[0],
-      name: match[1],
-    });
-  }
+		return eat(match[0])({
+			type: 'svelteTag',
+			value: match[0],
+			name: match[1],
+		});
+	}
 }
--- test/parsers.spec.js
+++ test/parsers.spec.js
@@ -2,23 +2,65 @@ import { parse_svelte_tag } from '../src/parsers';
 
 // I have no idea what the unified/ remark eat function returns but i need to fake it.
 const eat = value => node => ({
-  value,
-  node,
+	value,
+	node,
 });
 
 describe('parse_svelte_tag', () => {
-  test('it should correctly match and parse any svelte tag: component', () => {
-    const input = '<svelte:component />';
+	const svelte_tags = [
+		['component', 'svelte:component'],
+		['self', 'svelte:self'],
+		['window', 'svelte:window'],
+		['body', 'svelte:body'],
+		['options', 'svelte:options'],
+	];
 
-    const output = parse_svelte_tag(eat, input, false);
+	svelte_tags.forEach(([name, component]) => {
+		test(`it should it should correctly match and parse any svelte tag: ${name}`, () => {
+			expect(parse_svelte_tag(eat, `<${component} />`, false)).toEqual({
+				value: `<${component} />`,
+				node: {
+					value: `<${component} />`,
+					name,
+					type: 'svelteTag',
+				},
+			});
+		});
 
-    expect(output).toEqual({
-      value: '<svelte:component />',
-      node: {
-        value: '<svelte:component />',
-        name: 'component',
-        type: 'svelteTag',
-      },
-    });
-  });
+		test(`it should it should correctly match and parse any svelte tag ignoring whitespace: ${name}`, () => {
+			expect(parse_svelte_tag(eat, `  		  		<${component} />`, false)).toEqual({
+				value: `  		  		<${component} />`,
+				node: {
+					value: `  		  		<${component} />`,
+					name,
+					type: 'svelteTag',
+				},
+			});
+		});
+
+		test(`it should it should correctly match and parse any svelte tag regardless of attributes: ${name}`, () => {
+			const output = parse_svelte_tag(
+				eat,
+				`<${component} foo=bar quu="quux" hello on:click on:keypress={boo} />`,
+				false
+			);
+
+			expect(output).toEqual({
+				value: `<${component} foo=bar quu="quux" hello on:click on:keypress={boo} />`,
+				node: {
+					value: `<${component} foo=bar quu="quux" hello on:click on:keypress={boo} />`,
+					name,
+					type: 'svelteTag',
+				},
+			});
+		});
+	});
+
+	test('in silent mode, matches should return true', () => {
+		const input = '<svelte:component />';
+
+		const output = parse_svelte_tag(eat, input, true);
+
+		expect(output).toBe(true);
+	});
 });
