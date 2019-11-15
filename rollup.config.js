69f3ffab29b795c0f02f138e6715ae1ed0756cc7
pngwn <hello@pngwn.io>
2019-11-15 11:16:37 +0000

    Fix build for browser.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
+import builtins from 'rollup-plugin-node-builtins';
 
 import pkg from './package.json';
 
@@ -8,13 +9,20 @@ const opts = {
 	plugins: [resolve({ preferBuiltins: true }), commonjs(), json()],
 };
 
+console.log(pkg.browser, pkg.main, pkg.module);
+
 export default [
 	{
 		...opts,
-		input: 'src/main.ts',
+		input: 'src/index.js',
 		output: [
 			{ file: pkg.module, format: 'es', sourcemap: false },
 			{ file: pkg.main, format: 'cjs', sourcemap: false },
 		],
+	},
+	{
+		plugins: [...opts.plugins, builtins()],
+		input: 'src/index.js',
+		output: [{ file: pkg.browser, format: 'es', sourcemap: false }],
 	},
 ];
