e41304744652dba1b3794e0a84f20793b4c1499e
pngwn <hello@pngwn.io>
2019-11-17 19:30:07 +0000

    Fix weird es/require compat import. Again...

--- src/transformers/index.js
+++ src/transformers/index.js
@@ -2,7 +2,8 @@ import retext from 'retext';
 import smartypants from 'retext-smartypants';
 import visit from 'unist-util-visit';
 import yaml from 'js-yaml';
-import * as svelte from 'svelte/compiler';
+import svelte from 'svelte/compiler';
+
 const parse = svelte.parse || svelte.default.parse;
 
 // extract the yaml from 'yaml' nodes and put them in the vfil for later use
