87f3b30b7fc9936f5d9b451dd17f9df5fc06e63b
pngwn <hello@pngwn.io>
2020-08-26 18:40:00 +0100

    some types and things

---
+++ packages/svelte-parse/src/types_and_things.ts
@@ -0,0 +1,60 @@
+import { Point, Node } from 'svast';
+
+export const TAB = 9; // "\t"
+export const LINEFEED = 10; // "\n"
+export const SPACE = 32; // " "
+export const QUOTE = 34; // "'"
+export const APOSTROPHE = 39; // "'"
+export const DASH = 45; // "-"
+export const DOT = 46; // "."
+export const SLASH = 47; // "/"
+export const COLON = 58; // ":"
+export const OPEN_ANGLE_BRACKET = 60; // "<"
+export const EQUALS = 61; // "="
+export const CLOSE_ANGLE_BRACKET = 62; // ">"
+export const OPEN_BRACE = 123; // "{"
+export const CLOSE_BRACE = 125; // "}"
+
+export interface Result {
+	/**
+	 * The chomped string, what has been parsed. This is a substring of the input value.
+	 */
+	chomped: string;
+
+	/**
+	 * The unchomped string, what is still left to parse. This is a substring of the input value.
+	 */
+	unchomped: string;
+
+	/**
+	 * The AST node. The result of the parse.
+	 */
+	parsed: Node;
+	/**
+	 * The location in the document where the parse finished. This can be passed back into the parseNode function to maintain positional information on subsequent passes.
+	 */
+	position: Point;
+}
+
+export interface ParserOptions {
+	/**
+	 * The input value to be parsed
+	 */
+	value: string;
+	/**
+	 * The current position in the document
+	 */
+	currentPosition?: Point;
+	/**
+	 * The parser to use when parsing children, this defaults to `parseNode`
+	 */
+	childParser?: (options: ParserOptions) => Result;
+	/**
+	 * Are we currently in a block or are we currently inline?
+	 */
+	block?: boolean;
+	/**
+	 * I'm not really sure what this is for
+	 */
+	silent?: boolean;
+}
