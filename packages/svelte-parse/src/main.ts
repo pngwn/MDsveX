984f80cb251ee5645b413fac904e35ad18c5526f
pngwn <hello@pngwn.io>
2020-08-26 18:40:00 +0100

    some types and things

--- packages/svelte-parse/src/main.ts
+++ packages/svelte-parse/src/main.ts
@@ -1,1 +1,31 @@
-export default 'hello';
+import { Point, Node } from 'svast';
+
+import type { ParserOptions, Result } from './types_and_things';
+
+function default_eat(value: string) {
+	return function apply(node: Node): Node {
+		return node;
+	};
+}
+
+const pos: Point = {
+	line: 1,
+	column: 1,
+	offset: 0,
+};
+
+export function parseNode(opts: ParserOptions): Result {
+	const {
+		value,
+		currentPosition = pos,
+		block = true,
+		childParser = parseNode,
+	} = opts;
+
+	return {
+		chomped: 'asd',
+		unchomped: 'asd',
+		parsed: { type: 'hi', children: [] },
+		position: currentPosition,
+	};
+}
