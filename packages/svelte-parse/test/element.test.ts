3bbbe5395eec3b301682e0293d6f2edf18d9e928
pngwn <hello@pngwn.io>
2020-08-27 10:03:17 +0100

    Add types to tests output, parse self closing components and all void tags

--- packages/svelte-parse/test/element.test.ts
+++ packages/svelte-parse/test/element.test.ts
@@ -1,19 +1,60 @@
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 
+import { SvelteElement, SvelteComponent } from 'svast';
+
 import { parseNode } from '../src/main';
+import { void_els } from '../src/void_els';
 
 const element = suite('parse-element');
 
-element('parses a simple tag', () => {
+element('parses a self closing tag without attributes', () => {
 	const { parsed } = parseNode({ value: `<input />` });
 
-	assert.equal(parsed, {
+	assert.equal(parsed, <SvelteElement>{
 		type: 'svelteElement',
 		tagName: 'input',
 		selfClosing: true,
 		children: [],
 		properties: [],
+	});
+});
+
+element('parses a self closing component without attributes', () => {
+	const { parsed } = parseNode({ value: `<HelloFriend />` });
+
+	assert.equal(parsed, <SvelteComponent>{
+		type: 'svelteComponent',
+		tagName: 'HelloFriend',
+		selfClosing: true,
+		children: [],
+		properties: [],
+	});
+});
+
+void_els.forEach((el) => {
+	element(`parses all void tags without attributes: < ${el} >`, () => {
+		const { parsed } = parseNode({ value: `<${el} >` });
+
+		assert.equal(parsed, <SvelteElement>{
+			type: 'svelteElement',
+			tagName: el,
+			selfClosing: true,
+			children: [],
+			properties: [],
+		});
+	});
+});
+
+element.skip('parses a self closing tag with shorthand attribute', () => {
+	const { parsed } = parseNode({ value: `<input hello />` });
+
+	assert.equal(parsed, <SvelteElement>{
+		type: 'svelteElement',
+		tagName: 'input',
+		selfClosing: true,
+		children: [],
+		properties: [{}],
 	});
 });
 
