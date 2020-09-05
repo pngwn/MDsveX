093511bc5c175b9a8475f8181377c6b3b402a119
pngwn <hello@pngwn.io>
2020-09-05 16:44:55 +0100

    switch to real ts rollup plugin

--- packages/svelte-parse/rollup.config.js
+++ packages/svelte-parse/rollup.config.js
@@ -1,14 +1,15 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
-import sucrase from '@rollup/plugin-sucrase';
+// import sucrase from '@rollup/plugin-sucrase';
+import ts from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
 import pkg from './package.json';
 
 export default [
 	{
-		plugins: [sucrase({ transforms: ['typescript'] })],
+		plugins: [ts()],
 		input: 'src/main.ts',
 		output: [{ file: 'dist/main.js', format: 'cjs', sourcemap: false }],
 	},
