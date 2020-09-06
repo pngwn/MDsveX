0efaee8d94b36d2aea9903ab14c5c2b195a02644
pngwn <hello@pngwn.io>
2020-09-06 13:34:14 +0100

    walk

--- packages/svast-utils/test/walk.test.ts
+++ packages/svast-utils/test/walk.test.ts
@@ -270,4 +270,29 @@ svast_walk(
 	}
 );
 
+svast_walk('a more complex node', () => {
+	const leaf = { type: 'leaf' };
+	const has_child = { type: 'haschild1', children: [leaf] };
+	const has_child2 = { type: 'haschild2', children: [has_child] };
+	const has_child3 = { type: 'haschild3', children: [has_child2] };
+	const tree = {
+		type: 'hi',
+		children: [has_child3],
+	};
+
+	const _p: (Node | undefined)[] = [];
+	const _t: string[] = [];
+	walk(tree, (node, parent) => {
+		_p.push(parent);
+		_t.push(node.type);
+	});
+
+	assert.is(_p[0], undefined);
+	assert.is(_p[1], tree);
+	assert.is(_p[2], has_child3);
+	assert.is(_p[3], has_child2);
+	assert.is(_p[4], has_child);
+	assert.equal(_t, ['hi', 'haschild3', 'haschild2', 'haschild1', 'leaf']);
+});
+
 svast_walk.run();
