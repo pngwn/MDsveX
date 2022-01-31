764e443ad06079de3c93e662ddf81810c2772b77
Puru Vijay <awesomepuruvj@gmail.com>
2022-01-31 19:58:44 +0530

    defineConfig

--- packages/mdsvex/src/main.ts
+++ packages/mdsvex/src/main.ts
@@ -1,2 +1,8 @@
-export { mdsvex, compile } from './index';
+export { defineConfig as defineMDSveXConfig } from './define-config';
+export { compile, mdsvex } from './index';
 export { escape_svelty as escapeSvelte } from './transformers';
+export type {
+	MdsvexCompileOptions,
+	MdsvexLanguage,
+	MdsvexOptions,
+} from './types';
