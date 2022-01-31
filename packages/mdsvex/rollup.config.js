2dfc98693f4c7547cc4527629f7f68c5255cde27
pngwn <hello@pngwn.io>
2022-01-31 14:07:30 +0000

    actually fix deps this time

--- packages/mdsvex/rollup.config.js
+++ packages/mdsvex/rollup.config.js
@@ -1,12 +1,11 @@
-
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
 import sucrase from '@rollup/plugin-sucrase';
 import dts from 'rollup-plugin-dts';
 import replace from '@rollup/plugin-replace';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
 
 import pkg from './package.json';
 
