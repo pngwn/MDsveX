9822528b4176174cd885a53e0034b5c1c5bd4aee
pngwn <hello@pngwn.io>
2023-06-25 00:03:48 +0100

    export code_highlighter

--- packages/mdsvex/src/main.ts
+++ packages/mdsvex/src/main.ts
@@ -1,5 +1,5 @@
 export { defineConfig as defineMDSveXConfig } from './define-config';
-export { compile, mdsvex } from './index';
+export { compile, mdsvex, code_highlighter } from './index';
 export { escape_svelty as escapeSvelte } from './transformers';
 export type {
 	MdsvexCompileOptions,
