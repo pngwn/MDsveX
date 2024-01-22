ba39bfbc63695f55647bfc623f9df76b6868462f
pngwn <hello@pngwn.io>
2024-01-22 00:50:58 +0000

    fix site maybe

--- packages/_test/.svelte-kit/generated/client/nodes/0.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../../../node_modules/.pnpm/@sveltejs+kit@1.20.5_svelte@4.0.0_vite@4.3.9/node_modules/@sveltejs/kit/src/runtime/components/layout.svelte";
