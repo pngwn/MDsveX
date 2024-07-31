2b13e2edfb76451a3e910733aab2f75f8e8d686a
Moiri Gamboni <moiri.gamboni@proton.me>
2024-07-31 09:28:35 +0200

    lint

--- packages/mdsvex/globals.d.ts
+++ packages/mdsvex/globals.d.ts
@@ -1,15 +1,15 @@
 declare module '*.svx' {
-	import type { SvelteComponent } from 'svelte'
+	import type { SvelteComponent } from 'svelte';
 
-	export default class Comp extends SvelteComponent{}
+	export default class Comp extends SvelteComponent {}
 
-	export const metadata: Record<string, unknown>
+	export const metadata: Record<string, unknown>;
 }
 
 declare module '*.svelte.md' {
-	import type { SvelteComponent } from 'svelte'
+	import type { SvelteComponent } from 'svelte';
 
-	export default class Comp extends SvelteComponent{}
+	export default class Comp extends SvelteComponent {}
 
-	export const metadata: Record<string, unknown>
+	export const metadata: Record<string, unknown>;
 }
