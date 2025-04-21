e6667324b8df0476f97c93e3fb66efdcdbf47ad3
pngwn <hello@pngwn.io>
2025-04-21 11:27:37 +0100

    action

--- packages/test-app/playwright.config.ts
+++ packages/test-app/playwright.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'pnpm run build && pnpm run preview',
+		port: 4173,
 	},
-	testDir: 'e2e'
+	testDir: 'e2e',
 });
