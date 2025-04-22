9e4178180a5cd41f84cd14dba7903cb4705dbef3
pngwn <hello@pngwn.io>
2025-04-22 01:27:35 +0100

    fix fs failure

--- packages/mdsvex/rollup.config.js
+++ packages/mdsvex/rollup.config.js
@@ -5,16 +5,10 @@ import sucrase from '@rollup/plugin-sucrase';
 import dts from 'rollup-plugin-dts';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
-import polyfillNode from 'rollup-plugin-polyfill-node';
-
-const pkg = JSON.parse(readFileSync(resolve('package.json'), 'utf-8'));
 
 export default [
 	{
 		plugins: [
-			polyfillNode({
-				include: ['process'],
-			}),
 			node({ preferBuiltins: true }),
 			commonjs(),
 			json(),
---
+++ packages/test-app/src/Layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	const { children, ...rest } = $props();
+	$inspect(rest);
+</script>
+
+<p>am layout</p>
+<p>metadata title: {rest.title}</p>
+{@render children()}
